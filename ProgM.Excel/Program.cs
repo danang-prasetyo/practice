24c3dff635467843e58ba2ddb27555b40377ea56
danang-prasetyo <danang.adi.prasetyo@gmail.com>
2013-11-16 14:30:05 +0800

    Initial Upload

---
+++ ProgM.Excel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LinqToExcel;
+
+namespace ProgM.Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xl = new LinqToExcel.ExcelQueryFactory("C:\\Users\\Danang\\Desktop\\Book1.xlsx");
+            var ws = xl.GetWorksheetNames();
+            var cl = xl.GetColumnNames(ws.FirstOrDefault());
+            var rs = (from c in xl.WorksheetNoHeader(ws.FirstOrDefault())
+                                   select c).ToList();
+
+        }
+    }
+}
